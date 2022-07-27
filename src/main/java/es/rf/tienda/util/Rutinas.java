8060c086a4309888df74e4efe12b76086815e44b
franciscobenito <francisco.benito@qaracter.es>
2022-07-27 14:55:31 +0200

    commit mvn 27/07

---
+++ src/main/java/es/rf/tienda/util/Rutinas.java
@@ -0,0 +1,19 @@
+package es.rf.tienda.util;
+
+public class Rutinas {
+
+	public static Object addCampo(String salida, String nombreCampo2, String valorCampo2, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+
+	public static String addSalidaSencilla(String salida, String nCampo, String valor, String separador) {
+		if (salida!=null && salida.length()<0)
+			salida+= " "+separador+" ";
+		
+		return salida;
+	}
+}
